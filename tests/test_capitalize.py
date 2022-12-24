3013fd9df6d779ceb422c93e1d8aa82497e4b3ea
Pavel Belyavskiy <prostopasha@gmail.com>
2022-12-24 19:43:26 +0200

    Add 'assert'

--- tests/test_capitalize.py
+++ tests/test_capitalize.py
@@ -1,9 +1,4 @@
 from hexlet_asserts_tests.capitalize import capitalize
 
-if capitalize('hello') != 'Hello':
-    raise Exception('Функция работает неверно!')
-
-if capitalize('') != '':
-    raise Exception('Функция работает неверно!')
-
-print('Все тесты пройдены!')
+assert capitalize('hello') == 'Hello'
+assert capitalize('') == ''
